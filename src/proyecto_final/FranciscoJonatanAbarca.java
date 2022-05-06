40f1bd75bfbbc99ebc38ee0ade9c2be9587cf45f
[JhonatanRoque] <[franciscoabarca7392@gmail.com]>
2022-05-06 17:07:05 -0600

    Clase Francisco Jonatan Abarca

---
+++ src/proyecto_final/FranciscoJonatanAbarca.java
@@ -0,0 +1,9 @@
+package proyecto_final;
+
+
+public class FranciscoJonatanAbarca {
+    public static void main(String[] args) {
+        System.out.println("Nombre: ");
+        System.out.println("Francisco Jonatan Abarca Roque");
+    }
+}
