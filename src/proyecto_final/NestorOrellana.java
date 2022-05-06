e9fb86c6915de0d8dde9152c5ccea335aea35a0a
nestorclash <101343323+nestorclash@users.noreply.github.com>
2022-05-06 17:38:46 -0600

    Clase NestorOrellana

---
+++ src/proyecto_final/NestorOrellana.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_final;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class NestorOrellana {
+    public static void main(String[] args) {
+        System.out.println("Nombre: ");
+        System.out.println("Nestor Aaron Orellana Lopez");
+    }
+}
