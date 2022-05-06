1c4416de4ab418d360b550125e3d36c74b47d777
Criss7w7 <101343214+Criss7w7@users.noreply.github.com>
2022-05-06 17:55:32 -0600

    Clase Area

--- src/proyecto_final/CristinaArgueta.java
+++ src/proyecto_final/CristinaArgueta.java
@@ -7,6 +7,6 @@ public class CristinaArgueta {
 
     public static void main(String[] args) {
         System.out.println("Nombre: ");
-        System.out.println("Cristina Argueta");
+        System.out.println("Cristina Francisca Argueta");
     }
 }
